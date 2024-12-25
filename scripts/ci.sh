26a2b6b2ddd01abdd12d8e8b6f4ad0b6b3ec360f
axylol <156540890+axylol@users.noreply.github.com>
2024-12-25 03:17:53 -0800

    fix typos in github actions

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -2,11 +2,11 @@
 ./scripts/build.sh
 
 # Copy dll files if line.exe exists
-if [ -e "./build/msys-linemt5.dll" ]; then    
-    cp -f "./build/msys-linemt5.dll" "./dist"
+if [ -e "./build/msys-linemt4.dll" ]; then    
+    cp -f "./build/msys-linemt4.dll" "./dist"
     cp -f "C:\msys32\usr\bin\msys-z.dll" "./dist"
     cp -f "./lib/SDL2.dll" "./dist"
 
     # just strip the binary because its a release build
-    strip "./dist/msys-linemt5.dll"
+    strip "./dist/msys-linemt4.dll"
 fi
